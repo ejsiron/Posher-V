431620dc6c1993c6c4a6e20d4f2dd1333e6bce33
Eric Siron <ejsiron@users.noreply.github.com>
2019-02-12 10:37:14 -0600

    Update Get-Help to reference new GitHub Pages

--- Standalone/Update-VMIntegrationServices.ps1
+++ Standalone/Update-VMIntegrationServices.ps1
@@ -26,7 +26,7 @@ Author: Eric Siron
 Version 1.0, December 11, 2018
 Released under MIT license
 .LINK
-https://github.com/ejsiron/Posher-V/blob/master/docs/Update-VMIntegrationServices.md
+https://ejsiron.github.io/Posher-V/Update-VMIntegrationServices
 #>
 [CmdletBinding(DefaultParameterSetName='Default')]
 param(
