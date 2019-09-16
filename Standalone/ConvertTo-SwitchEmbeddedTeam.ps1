b1282350f3fe46cd86dc58e6717bde03716b96d9
Eric Siron <ejsiron@users.noreply.github.com>
2019-09-16 15:44:44 -0500

    Update ConvertTo-SwitchEmbeddedTeam.ps1

--- Standalone/ConvertTo-SwitchEmbeddedTeam.ps1
+++ Standalone/ConvertTo-SwitchEmbeddedTeam.ps1
@@ -55,7 +55,7 @@ param(
 	[Parameter(Position = 1, ParameterSetName = 'ByName')][String[]]$Name = @(''),
 	[Parameter(Position = 1, ParameterSetName = 'ByID', Mandatory = $true)][System.Guid[]]$Id,
 	[Parameter(Position = 1, ParameterSetName = 'BySwitchObject', Mandatory = $true)][Microsoft.HyperV.PowerShell.VMSwitch[]]$VMSwitch,
-	[Parameter(Position = 2)][String[]]$NewName,
+	[Parameter(Position = 2)][String[]]$NewName = @(),
 	[Parameter()][Switch]$UseDefaults,
 	[Parameter()][Switch]$EnableIOV,
 	[Parameter()][Switch]$EnablePacketDirect,
@@ -71,36 +71,46 @@ BEGIN
 	function New-NetAdapterObjectPack
 	{
 		param(
-			[Parameter(Mandatory = $true)][Microsoft.HyperV.PowerShell.VMInternalNetworkAdapter]$VNIC
+			[Parameter(Mandatory = $true)]$VNIC
 		)
-		$ObjectPack = New-Object psobject
-		Add-Member -InputObject $ObjectPack -Name VnicData -TypeName Microsoft.HyperV.PowerShell.VMInternalNetworkAdapter -Value $VNIC
-		Add-Member -InputObject $ObjectPack -Name VlanData -TypeName Microsoft.HyperV.PowerShell.VMNetworkAdapterVlanSetting -Value (
-			Get-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapter $VNIC
-		)
-		Add-Member -InputObject $ObjectPack
+		
+		$VLANData = Get-VMNetworkAdapterVlan -VMNetworkAdapter $VNIC
+		$VnicCim = Get-CimInstance -Namespace root/virtualization/v2 -ClassName Msvm_InternalEthernetPort -Filter ('Name="{0}"' -f $VNIC.AdapterId)
+		$VnicLanEndpoint1 = Get-CimAssociatedInstance -InputObject $VnicCim -ResultClassName Msvm_LANEndpoint
+		$NetAdapter = Get-CimInstance -ClassName Win32_NetworkAdapter -Filter ('GUID="{0}"' -f $VnicLANEndpoint1.Name.Substring(($VnicLANEndpoint1.Name.IndexOf('{'))))
+		$NetAdapterConfiguration = Get-CimAssociatedInstance -InputObject $NetAdapter -ResultClassName Win32_NetworkAdapterConfiguration
+		$MinimumBandwidthAbsolute = 0
+		$MinimumBandwidthWeight = 0
+		$MaximumBandwidth = 0
 
+		if ($VNIC.BandwidthSetting -ne $null)
+		{
+			$MinimumBandwidthAbsolute = $VNIC.BandwidthSetting.MinimumBandwidthAbsolute
+			$MinimumBandwidthWeight = $VNIC.BandwidthSetting.MinimumBandwidthWeight
+			$MaximumBandwidth = $VNIC.BandwidthSetting.MaximumBandwidth
+		}
+
+		$ObjectPack = New-Object psobject
+		$OBParams = @{InputObject = $ObjectPack; MemberType = 'NoteProperty' }
+		Add-Member @OBParams -Name NicName -TypeName System.String -Value $VNIC.Name
+		Add-Member @OBParams -Name SwitchName -TypeName System.String -Value $VNIC.SwitchName
+		Add-Member @OBParams -Name MacAddress -TypeName System.String -Value $VNIC.MacAddress
+		Add-Member @OBParams -Name MinimumBandwidthAbsolute -TypeName System.Int64 -Value $MinimumBandwidthAbsolute
+		Add-Member @OBParams -Name MinimumBandwidthWeight -TypeName System.Int64 -Value $MinimumBandwidthWeight
+		Add-Member @OBParams -Name MaximumBandwidth -TypeName System.Int64 -Value $MaximumBandwidth
+		Add-Member @OBParams -Name VLANID -TypeName System.Int32 -Value $VLANData.AccessVlanId
+		Add-Member @OBParams -Name AdapterConfiguration -TypeName Microsoft.Management.Infrastructure.CimInstance -Value $NetAdapterConfiguration
+
+		$ObjectPack
 	}
 }
-
-# DHCP or no
-# IP
-# gateways
-# DNS addresses
-# DNS search suffixes
-# DNS suffixes
-# Register
-# Use suffix in registration
-# WINS
-# LMHOSTS
-# NetBIOS over TCP/IP
-
 
 PROCESS
 {
 	$Switches = New-Object System.Collections.ArrayList
+	$HostVNICData = New-Object System.Collections.ArrayList
 
-	switch (PSCmdlet.ParameterSetName)
+	switch ($PSCmdlet.ParameterSetName)
 	{
 		'ByID'
 		{
@@ -116,7 +126,7 @@ PROCESS
 			$NameList.AddRange($Name.ForEach( { $_.Trim() }))
 			if ($NameList.Contains('') -or $NameList.Contains('*'))
 			{
-				$Switches = Get-VMSwitch
+				$Switches.AddRange(@(Get-VMSwitch))
 			}
 			else
 			{
@@ -124,7 +134,25 @@ PROCESS
 			}
 		}
 	}
-	$Switches = Select-Object -InputObject $Switches -Unique
+	if ($Switches.Count)
+	{
+		$Switches = @(Select-Object -InputObject $Switches -Unique)
+	}
+	else
+	{
+		throw('No virtual switches match the provided criteria')
+	}
+
+	Write-Progress -Activity 'Pre-flight' -Status 'Verifying operating system version' -PercentComplete 5
+	Write-Verbose -Message 'Verifying operating system version'
+	$OSVersion = [System.Version]::Parse((Get-CimInstance -ClassName Win32_OperatingSystem).Version)
+	if ($OSVersion.Major -lt 10)
+	{
+		throw('Switch-embedded teams not supported on host operating system versions before 2016')
+	}
+
+	Write-Progress -Activity 'Pre-flight' -Status 'Loading virtual switches' -PercentComplete 15
+
 	if ($NewName.Count -gt 0 -and $NewName.Count -ne $Switches.Count)
 	{
 		$SwitchNameMismatchMessage = 'Switch count ({0}) does not match NewName count ({1}).' -f $Switches.Count, $NewName.Count
@@ -132,48 +160,76 @@ PROCESS
 		{
 			$SwitchNameMismatchMessage += ' If you wish to rename some switches but not others, specify an empty string for the switches to leave.'
 		}
-		Write-Error -Message $SwitchNameMismatchMessage
+		throw($SwitchNameMismatchMessage)
 	}
-	for ($i = 0; $i -lt $Switches.Count; $i++)
+
+	Write-Progress -Activity 'Pre-flight' -Status 'Validating virtual switch configurations' -PercentComplete 25
+	foreach ($Switch in $Switches)
 	{
-		Write-Verbose -Message ('Verifying that switch "{0}" is external' -f $Switches[$i].Name)
-		if ($Switches[$i].SwitchType -ne [Microsoft.HyperV.PowerShell.VMSwitchType]::External)
+		try
 		{
-			Write-Warning -Message ('Switch "{0}" is not external, skipping' -f $Switches[$i].Name)
+			Write-Progress -Activity ('Validating virtual switch "{0}"' -f $Switch.Name) -Status 'Switch is external' -PercentComplete 25
+			Write-Verbose -Message ('Verifying that switch "{0}" is external' -f $Switch.Name)
+			if ($Switch.SwitchType -ne [Microsoft.HyperV.PowerShell.VMSwitchType]::External)
+			{
+				Write-Warning -Message ('Switch "{0}" is not external, skipping' -f $Switch.Name) -WarningAction Stop
+			}
+
+			Write-Progress -Activity ('Validating virtual switch "{0}"' -f $Switch.Name) -Status 'Switch is not a SET' -PercentComplete 50
+			Write-Verbose -Message ('Verifying that switch "{0}" is not already a SET' -f $Switch.Name)
+			if ($Switch.EmbeddedTeamingEnabled)
+			{
+				Write-Warning -Message ('Switch "{0}" already uses SET, skipping' -f $Switch.Name) -WarningAction Stop
+			}
+
+			Write-Progress -Activity ('Validating virtual switch "{0}"' -f $Switch.Name) -Status 'Switch uses LBFO' -PercentComplete 75
+			Write-Verbose -Message ('Verifying that switch "{0}" uses an LBFO team' -f $Switch.Name)
+			$TeamAdapter = Get-CimInstance -Namespace root/StandardCimv2 -ClassName MSFT_NetLbfoTeamNic -Filter ('InterfaceDescription="{0}"' -f $Switch.NetAdapterInterfaceDescription)
+			if ($TeamAdapter -eq $null)
+			{
+				Write-Warning -Message ('Switch "{0}" does not use a team, skipping' -f $Switch.Name) -WarningAction Stop
+			}
+			if ($TeamAdapter.VlanID)
+			{
+				Write-Warning -Message ('Switch "{0}" is bound to a team NIC with a VLAN assignment, skipping' -f $Switch.Name) -WarningAction Stop
+			}	
+		}
+		catch
+		{
 			continue
+		}
+		finally
+		{
+			Write-Progress -Activity ('Validating virtual switch "{0}"' -f $Switch.Name) -Completed
 		}
 
-		Write-Verbose -Message ('Verifying that switch "{0}" is not already a SET' -f $Switches[$i].Name)
-		if (-not $Switches[$i].EmbeddedTeamingEnabled)
-		{
-			Write-Warning -Message ('Switch "{0}" already uses SET, skipping' -f $Switches[$i].Name)
-			continue
-		}
-
-		# msvm_internalethernetport to msvm_lanendpoint; Name links to adapter guid
-		Write-Verbose -Message ('Verifying that switch "{0}" uses a standard team' -f $Switches[$i].Name)
-		$AttachedAdapter = Get-NetAdapter -InterfaceDescription $Switches[$i].NetAdapterInterfaceDescription
-		$TeamCIMAdapter = Get-CimInstance -Namespace root/StandardCimv2 -ClassName MSFT_NetLbfoTeamNic -Filter ('InstanceID="{{{0}}}"' -f ($Switches[$i].NetAdapterInterfaceGuid).Guid.ToUpper())
-		if ($TeamCIMAdapter -eq $null)
-		{
-			Write-Warning -Message ('Switch "{0}" does not use a team, skipping' -f $Switches[$i].Name)
-			continue
-		}
-		if ($TeamCIMAdapter.VlanID)
-		{
-			Write-Warning -Message ('Switch "{0}" is bound to a team NIC with a VLAN assignment, skipping' -f $Switches[$i].Name)
-			continue
-		}
-
+		Write-Progress -Activity ('Loading information from virtual switch "{0}"' -f $Switch.Name) -Status 'Team NIC' -PercentComplete 25
 		Write-Verbose -Message 'Loading team'
-		$Team = Get-NetLbfoTeam -TeamNicForTheTeam $TeamCIMAdapter
+		$Team = Get-CimAssociatedInstance -InputObject $TeamAdapter -ResultClassName MSFT_NetLbfoTeam
 		
+		Write-Progress -Activity ('Loading information from virtual switch "{0}"' -f $Switch.Name) -Status 'Guest virtual adapters' -PercentComplete 50
 		Write-Verbose -Message 'Loading VM adapters connected to this switch'
-		$GuestVNICs = Get-VMNetworkAdapter -VMName * | Where-Object -Property SwitchName -EQ $Switches[$i].Name
+		$GuestVNICs = Get-VMNetworkAdapter -VMName * | Where-Object -Property SwitchName -EQ $Switch.Name
 
+		Write-Progress -Activity ('Loading information from virtual switch "{0}"' -f $Switch.Name) -Status 'Host virtual adapters' -PercentComplete 75
 		Write-Verbose -Message 'Loading management adapters connected to this switch'
-		$HostVNICs = Get-VMNetworkAdapter -ManagementOS -SwitchName $Switches[$i].Name
+		$HostVNICs = Get-VMNetworkAdapter -ManagementOS -SwitchName $Switch.Name
 
+		Write-Verbose -Message 'Gathering management OS virtual NIC information'
+		$HostVNICData.AddRange((ForEach-Object -InputObject $HostVNICs -Process { New-NetAdapterObjectPack -VNIC $_ } ))
 
+		$HostVNICData
 	}
 }
+
+# DHCP or no
+# IP
+# gateways
+# DNS addresses
+# DNS search suffixes
+# DNS suffixes
+# Register
+# Use suffix in registration
+# WINS
+# LMHOSTS
+# NetBIOS over TCP/IP
